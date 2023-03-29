2a889132f27d0ff9ab59f03d0b2da6d853ec741f
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-03-29 17:35:09 -0500

    Write queries (using JOIN) to answer the given  questions:

--- queries.sql
+++ queries.sql
@@ -7,21 +7,21 @@ WHERE name LIKE '%mon';
 SELECT *
 FROM animals
 WHERE EXTRACT(
-		YEAR
-		FROM date_of_birth
-	) BETWEEN 2016 AND 2019;
+        YEAR
+        FROM date_of_birth
+    ) BETWEEN 2016 AND 2019;
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
 SELECT *
 FROM animals
 WHERE neutered = true
-	AND escape_attempts < 3;
+    AND escape_attempts < 3;
 -- List the date of birth of all animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth
 FROM animals
 WHERE name IN('Agumon', 'Pikachu');
 -- List name and escape attempts of animals that weigh more than 10.5kg
 SELECT name,
-	escape_attempts
+    escape_attempts
 FROM animals
 WHERE weight_kg > 10.5;
 -- Find all animals that are neutered.
@@ -36,7 +36,6 @@ WHERE name != 'Gabumon';
 SELECT *
 FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
-
 -- new Queries day 2
 -- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
 start transaction;
@@ -49,13 +48,11 @@ ROLLBACK to B1;
 select *
 from animals;
 commit;
-
 start transaction;
 UPDATE animals
 SET species = 'digimon'
-WHERE name LIKE'%mon';
+WHERE name LIKE '%mon';
 commit;
-
 -- Verify that the species column was updated.
 start transaction;
 UPDATE animals
@@ -63,9 +60,7 @@ SET species = 'pokemon'
 WHERE species = '';
 commit;
 -- Verify that the species column was updated.
-
 -- Inside a transaction delete all records in the animals table, then roll back the transaction. After the rollback verify if all records in the animals table still exists. After that, you can start breathing as usual
-
 start transaction;
 savepoint B3;
 DELETE FROM animals;
@@ -73,73 +68,81 @@ DELETE FROM animals;
 start transaction;
 ROLLBACK to B3;
 commit;
-
 -- Delete all animals born after Jan 1st, 2022.
 -- Create a savepoint for the transaction.
 start transaction;
 savepoint B4;
 DELETE FROM animals
 WHERE date_of_birth > '2022-01-01';
-
 -- Update all animals' weight to be their weight multiplied by -1.
 start transaction;
 UPDATE animals
 SET weight_kg = weight_kg * -1;
-
 -- Rollback to the savepoint
 start transaction;
 ROLLBACK to B4;
 commit;
-
 -- Update all animals' weights that are negative to be their weight multiplied by -1.
 start transaction;
 UPDATE animals
 SET weight_kg = weight_kg * -1
 WHERE weight_kg < 0;
 commit;
-
 -- Write queries to answer the following questions:
 -- How many animals are there?
-SELECT 
-    COUNT(name)
-FROM
-    animals; -- 11 animals
-
+SELECT COUNT(name)
+FROM animals;
+-- 11 animals
 -- How many animals have never tried to escape?
-SELECT 
-    COUNT(escape_attempts)
-FROM
-    animals
-WHERE escape_attempts <= 0; -- THE ANSWER IS 2
-
+SELECT COUNT(escape_attempts)
+FROM animals
+WHERE escape_attempts <= 0;
+-- THE ANSWER IS 2
 -- What is the average weight of animals?
-SELECT 
-    AVG(weight_kg)
-FROM
-    animals; -- the average weight IS 16.1363636363636364
-
+SELECT AVG(weight_kg)
+FROM animals;
+-- the average weight IS 16.1363636363636364
 -- Who escapes the most, neutered or not neutered animals?
 SELECT neutered,
-SUM(escape_attempts) total_attempts 
-FROM animals 
-GROUP BY neutered; -- neutered 24 or not neutered 4
-
+    SUM(escape_attempts) total_attempts
+FROM animals
+GROUP BY neutered;
+-- neutered 24 or not neutered 4
 -- What is the minimum and maximum weight of each type of animal?
 SELECT species,
-MIN(weight_kg) AS min_weight, 
-MAX(weight_kg) max_weight 
-FROM animals 
-GROUP BY species; -- species | min_weight | max_weight
-                  ---------+------------+------------
-                  -- pokemon |         11 |         22
-                  -- digimon |        5.7 |         45
-
+    MIN(weight_kg) AS min_weight,
+    MAX(weight_kg) max_weight
+FROM animals
+GROUP BY species;
+-- species | min_weight | max_weight
+---------+------------+------------
+-- pokemon |         11 |         22
+-- digimon |        5.7 |         45
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT species, 
-AVG(escape_attempts) AS av_escape_attempts 
-FROM animals 
-WHERE EXTRACT(YEAR FROM date_of_birth) 
-BETWEEN 1990 AND 2000 
-GROUP BY species; --  species | av_escape_attempts
-                  ---------+--------------------
-                  -- pokemon | 3.0000000000000000
+SELECT species,
+    AVG(escape_attempts) AS av_escape_attempts
+FROM animals
+WHERE EXTRACT(
+        YEAR
+        FROM date_of_birth
+    ) BETWEEN 1990 AND 2000
+GROUP BY species;
+--  species | av_escape_attempts
+---------+--------------------
+-- pokemon | 3.0000000000000000
+
+-- Day 3 update -------
+-- Write queries (using JOIN) to answer the following questions:
+-- What animals belong to Melody Pond?
+
+-- List of all animals that are pokemon (their type is Pokemon).
+
+-- List all owners and their animals, remember to include those that don't own any animal.
+
+-- How many animals are there per species?
+
+-- List all Digimon owned by Jennifer Orwell.
+
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+
+-- Who owns the most animals?
--- schema.sql
+++ schema.sql
@@ -1,53 +1,37 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-  id int primary key GENERATED ALWAYS AS IDENTITY ,
+  id int primary key GENERATED ALWAYS AS IDENTITY,
   name varchar(100),
   date_of_birth date,
   escape_attempts int,
   neutered boolean,
   weight_kg decimal,
-  );
+);
+ALTER TABLE animals
+ADD species varchar(100);
 
- ALTER TABLE animals ADD species varchar(100);
-
- -- Day 3 --- Vet clinic database: query multiple tables
- -- create owners table
- CREATE TABLE owners (
-  id int primary key GENERATED ALWAYS AS IDENTITY ,
+-- Day 3 --- Vet clinic database: query multiple tables
+-- create owners table
+CREATE TABLE owners (
+  id int primary key GENERATED ALWAYS AS IDENTITY,
   full_name varchar(255),
   age int
- );
-
-  -- create species table
- CREATE TABLE species (
-  id int primary key GENERATED ALWAYS AS IDENTITY ,
+);
+-- create species table
+CREATE TABLE species (
+  id int primary key GENERATED ALWAYS AS IDENTITY,
   name varchar(255)
- );
-
- -- Modify animals table:
+);
+-- Modify animals table:
 -- Remove column species
 ALTER TABLE animals DROP COLUMN species;
-
- -- Add species_id column to animals table
-ALTER TABLE animals ADD species_id INT;
-
+-- Add species_id column to animals table
 ALTER TABLE animals
-ADD CONSTRAINT fk_species
-FOREIGN KEY (species_id)
-REFERENCES species (id)
-ON DELETE CASCADE;
-
- -- Add owner_id column to animals table
-ALTER TABLE animals ADD owner_id INT;
-
+ADD species_id INT;
 ALTER TABLE animals
-ADD CONSTRAINT fk_owners
-FOREIGN KEY (owner_id)
-REFERENCES owners (id)
-ON DELETE CASCADE;
-
-
-
-
-
+ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE;
+-- Add owner_id column to animals table
+ALTER TABLE animals
+ADD owner_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
